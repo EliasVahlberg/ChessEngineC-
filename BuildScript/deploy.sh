24c4af8cd321a24875cdc1091513f916a98ddbea
EliasVahlberg <elias.vahlberg.2@gmail.com>
2021-08-16 19:36:17 +0200

    Add Build Automation
    Add Build Automation

---
+++ BuildScript/deploy.sh
@@ -0,0 +1,4 @@
+echo Starting Build Procedure
+'C:\Program Files\Unity\Hub\Editor\2020.3.2f1\Editor\Unity.exe' -quit -batchmode -projectPath ../ -executeMethod BuildScript.PreformBuild
+
+echo Ended Build Procedure
