73b3cef92e3d740f751008ac252b9c5bdcc2fb76
EliasVahlberg <elias.vahlberg.2@gmail.com>
2021-08-16 19:53:09 +0200

    Change path in build script
    Change path in build script

--- Assets/SRC/BuildAutomation/BuildScript.cs
+++ Assets/SRC/BuildAutomation/BuildScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public static class BuildScript
 {
-    private const string BUILDS_FOLDER_PATH = @"C:\Users\elias\SimpleChessV2\Builds";
+    private const string BUILDS_FOLDER_PATH = @"C:\Users\elias\SimpleChessV2\Builds\";
     private const string BUILDS_FOLDER_PATH_UNITY = "./Builds/";
     private const string BUILD_FOLDER_NAME = "Build_V1.5";
     private const string PLATFORM_SUBFOLDER_MAC = "/Windows_x86_64";
@@ -21,22 +21,21 @@ public static class BuildScript
         System.IO.Directory.CreateDirectory(path + PLATFORM_SUBFOLDER_MAC);
         System.IO.Directory.CreateDirectory(path + PLATFORM_SUBFOLDER_WINDOWS);
         System.IO.Directory.CreateDirectory(path + PLATFORM_SUBFOLDER_LINUX);
-
         BuildPipeline.BuildPlayer(
             scenes,
-            unityPath + PLATFORM_SUBFOLDER_MAC,
+            unityPath + PLATFORM_SUBFOLDER_MAC + "/" + PLATFORM_SUBFOLDER_MAC + ".app",
             BuildTarget.StandaloneOSX,
             BuildOptions.None);
 
         BuildPipeline.BuildPlayer(
         scenes,
-        unityPath + PLATFORM_SUBFOLDER_WINDOWS,
+        unityPath + PLATFORM_SUBFOLDER_WINDOWS + "/" + PLATFORM_SUBFOLDER_WINDOWS + ".exe",
         BuildTarget.StandaloneWindows64,
         BuildOptions.None);
 
         BuildPipeline.BuildPlayer(
             scenes,
-            unityPath + PLATFORM_SUBFOLDER_LINUX,
+            unityPath + PLATFORM_SUBFOLDER_LINUX + "/" + PLATFORM_SUBFOLDER_LINUX + ".x86_64",
             BuildTarget.StandaloneLinux64,
             BuildOptions.None);
     }
